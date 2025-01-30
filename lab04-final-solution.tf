102004769e5a7c5724a784abe5c349d2324cf719
Sekar Shanmugam <sekar3s@gmail.com>
2025-01-29 22:54:48 -0600

    Update lab04-final-solution.tf

--- lab04-final-solution.tf
+++ lab04-final-solution.tf
@@ -1,3 +1,5 @@
+# main.tf
+
 terraform {
   required_providers {
     azurerm = {
@@ -142,59 +144,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   depends_on = [data.azurerm_resource_group.main, azurerm_virtual_network.main]
 }
-
-locals {
-  scriptWorkingDir = "/home/${local.vm.user_name}/"
-}
-
-resource "null_resource" remoteExecProvisioner {
-
-provisioner "file" {
-    source      = "./test.sh"
-    destination = "${local.scriptWorkingDir}/test.sh"
-  }
-
-connection {
-    host     = azurerm_public_ip.vm.ip_address
-    type     = "ssh"
-    user     = local.vm.user_name
-    password = data.azurerm_key_vault_secret.main.value
-    agent    = "false"
-  }
-
-  triggers = {
-    src_hash = "${data.archive_file.init.output_sha}"
-  }
-
-  depends_on = [azurerm_linux_virtual_machine.vm, azurerm_network_security_group.nsg]
-}
-
-resource "azurerm_virtual_machine_extension" "main" {
-  virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
-  name                 = "hostname"
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-  {
-    "commandToExecute": "chmod +x ${local.scriptWorkingDir}/test.sh; sudo apt-get install dos2unix; dos2unix ${local.scriptWorkingDir}/test.sh; /bin/bash ${local.scriptWorkingDir}/test.sh >> ${local.scriptWorkingDir}/helloworld.log"
-  }
-  SETTINGS
-
-  tags = {
-    environment = local.environment
-  }
-
-  depends_on = [azurerm_linux_virtual_machine.vm, azurerm_network_security_group.nsg, null_resource.remoteExecProvisioner]
-}
-
-data "archive_file" "init" {
-  type        = "zip"
-  source_file = "${path.module}/test.sh"
-  output_path = "${path.module}/test.zip"
-}
-
 
 # Variables.tf
 

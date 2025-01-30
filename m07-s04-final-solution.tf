f5b9e8d763bf31c41e6bc60a32c24b42d1094394
Sekar Shanmugam <sekar3s@gmail.com>
2025-01-29 22:29:04 -0600

    Update m07-s04-final-solution.tf

--- m07-s04-final-solution.tf
+++ m07-s04-final-solution.tf
@@ -92,7 +92,7 @@ resource "azurerm_network_interface" "vm" {
 }
 
 resource "azurerm_public_ip" "vm" {
-  name                = "mypip"
+  name                = "${local.environment}-pip"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   allocation_method   = "Static"
